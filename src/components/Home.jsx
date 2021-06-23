6d18f68b6417952a4396106b1b3a91f88f94341f
mayurkanade44 <mayurkanade44@gmail.com>
2021-06-23 14:52:46 +0530

    search

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,31 +6,47 @@ const url = "https://api.covid19india.org/data.json";
 
 const Home = () => {
   const [data, setData] = useState([]);
-  const [error, setError] = useState(false);
   const [states, setStates] = useState("Total");
   const [filtered, setFiltered] = useState([]);
   const [text, setText] = useState("");
+  const [sort, setSort] = useState([]);
 
   const fetchData = async () => {
     try {
       const resp = await fetch(url);
       const data = await resp.json();
       setData(data.statewise);
+      setSort(data.statewise);
     } catch (error) {
-      setError(true);
+      console.log(error);
     }
   };
 
-  
-  
+  const onUpdate = (e) => {
+    setText(e.target.value);
+  };
 
   const updateFilter = (m) => {
     setFiltered(data.filter((d) => d.state === m));
   };
 
+  const search = () => {
+    let temp = [...data];
+    if (text) {
+      temp = temp.filter((t) => {
+        return t.state.toLowerCase().startsWith(text);
+      });
+    }
+    return setSort(temp);
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    search();
+  }, [text]);
 
   useEffect(() => {
     updateFilter(states);
@@ -48,7 +64,12 @@ const Home = () => {
           </div>
         </div>
         <div className="col-lg-3">
-          <StateList states={data} updateFilter={updateFilter} />
+          <StateList
+            states={sort}
+            updateFilter={updateFilter}
+            text={text}
+            onUpdate={onUpdate}
+          />
         </div>
       </div>
     </>
